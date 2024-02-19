c889855204936b4eac434e7791ebe4c9a6408985
nashihu <ahmad.n.id@gmail.com>
2024-02-19 16:28:25 +0700

    set profile

--- lib/pages/profil/profile_page.dart
+++ lib/pages/profil/profile_page.dart
@@ -9,7 +9,9 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(height: Get.statusBarHeight / Get.pixelRatio),
           Stack(
+            alignment: Alignment.bottomCenter,
             children: [
               Container(
                 decoration: const BoxDecoration(
@@ -20,20 +22,67 @@ class ProfilePage extends StatelessWidget {
                   ),
                 ),
                 padding: const EdgeInsets.all(16),
-                margin: const EdgeInsets.only(bottom: 75),
-                height: Get.height * .2,
+                // margin: const EdgeInsets.only(bottom: 75),
+                height: Get.width * .5,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     ClipRRect(
                       borderRadius: const BorderRadius.all(Radius.circular(50)),
                       child: Image.network(
-                        'https://lh3.googleusercontent.com/a-/ALV-UjVEaDUEUo9JlvcGe99OYtlhjJH0ZUKXirRWVnTV0lLEL1=s360-s',
+                        'https://lh3.googleusercontent.com/a-/ALV-UjVEaDUEUo9JlvcGe99OYtlhjJH0ZUKXirRWVnTV0lLEL18=s360-p',
+                        height: Get.width * .25,
+                      ),
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: Get.width * .25,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              'Ruth Karolina Sirait',
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 32,
+                              ),
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                  color: Color(0xFF66BBB1),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(12))),
+                              padding: const EdgeInsets.all(12),
+                              child: const Text('Atur Akun'),
+                            ),
+                          ],
+                        ),
                       ),
                     )
                   ],
                 ),
-              )
+              ),
+              const Positioned(
+                bottom: 32,
+                left: 16,
+                right: 16,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'EMAIL: ruthsirait25@gmail.com',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    Text(
+                      'No. Telepon: 6281264086157',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ],
+                ),
+              ),
             ],
           )
         ],
